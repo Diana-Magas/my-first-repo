1641277d9437fad1591914c193f86ee1e2659bf5
Diana-Magas <155903433+Diana-Magas@users.noreply.github.com>
2024-03-11 16:04:59 +0200

    commit

--- practical-work-7/script.js
+++ practical-work-7/script.js
@@ -1,9 +1,9 @@
-function Triangular(a = 4, b = 5, c = 6) {
+function Triangular(a = 1, b = 2, c = 3) {
     return { a, b, c };
 }
 
 console.log(Triangular());
-console.log(Triangular(1, 2, 3));
+console.log(Triangular(4, 5, 6));
 console.log(Triangular(7, 8, 9));
 
 function PiMultiplier(num) {
@@ -25,15 +25,28 @@ function Painter(color) {
         console.log(`Color - ${color} and ${(obj.type === undefined) ? "No 'type' property occurred!" : "type: " + obj.type}`);
     };
 }
+let PaintBlue = Painter("blue");
+let PaintRed = Painter("red");
+let PaintYellow = Painter("yellow");
 
-let PaintBlue = Painter('blue');
-let PaintRed = Painter('red');
-let PaintYellow = Painter('yellow');
+let bmw = {
+    maxSpeed: 280,
+    type: "Sportcar",
+    color: "magenta",
+};
 
-let BMW = { maxSpeed: 280 ,  type: 'Sportcar',color: 'magenta'};
-let Audi = { type: 'Sportcar', avgSpeed: 90,'load capacity': 2400 };
-let Ford = { maxSpeed: 180,color: "purple", isCar: true };
+let audi = {
+    type: "Truck",
+    "avg speed": 280,
+    "load capacity": 2400,
+};
 
-PaintBlue(BMW);
-PaintRed(Audi);
-PaintYellow(Ford);
+let ford = {
+    maxSpeed: 180,
+    color: "purple",
+    isCar: true,
+};
+
+PaintBlue(bmw);
+PaintRed(audi);
+PaintYellow(ford);
