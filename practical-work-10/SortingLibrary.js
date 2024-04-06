d8a66d465fdd199d897a4b4b3af08c0785da04e5
Diana-Magas <155903433+Diana-Magas@users.noreply.github.com>
2024-04-06 20:02:47 +0300

    Update SortingLibrary.js

--- practical-work-10/SortingLibrary.js
+++ practical-work-10/SortingLibrary.js
@@ -1,81 +1,107 @@
-(function (global) {
-    const SortLibrary = {};
+(function(global) {
+    function getRandomInt(max = 100) {
+        return Math.floor(Math.random() * max);
+    }
 
-    const isSorted = function (array, reversed) {
-        return reversed
-            ? array.every((value, index, item) => !index || item[index - 1] >= value)
-            : array.every((value, index, item) => !index || item[index - 1] <= value);
+
+    function generateArray(n) {
+        let array = [];
+        for (let index = 0; index < n; index++) {
+            array[index] = getRandomInt();
+        }
+        return array;
+    }
+
+    function isSorted(array, ascending) {
+        const compare = ascending ? (a, b) => a <= b : (a, b) => a >= b;
+        for (let i = 1; i < array.length; i++) {
+            if (!compare(array[i - 1], array[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function swap(array, i, j) {
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    SortLibrary.bubbleSort = function(array, ascending = true) {
+        const compare = ascending ? (a, b) => a > b : (a, b) => a < b;
+        let compareCount = 0;
+        let swapCount = 0;
+        let sorted = false;
+        for (let i = 0; i < array.length - 1 && !sorted; i++) {
+            sorted = true;
+            for (let j = 0; j < array.length - 1 - i; j++) {
+                compareCount++;
+                if (compare(array[j], array[j + 1])) {
+                    swap(array, j, j + 1);
+                    swapCount++;
+                    sorted = false;
+                }
+            }
+        }
+        console.log(`Bubble sort compare count: ${compareCount}`);
+        console.log(`Bubble sort swap count: ${swapCount}`);
+        return array;
     };
 
-    function getCompare(reversed) {
-        return reversed ? (left, right) => left < right : (left, right) => left > right;
-    }
-
-    SortLibrary.bubbleSort = function (array, reversed) {
-        const compare = getCompare(reversed);
+    SortLibrary.selectionSort = function(array, ascending = true) {
+        const compare = ascending ? (a, b) => a > b : (a, b) => a < b;
         let compareCount = 0;
         let swapCount = 0;
-        while (!isSorted(array, reversed)) {
-            for (let index = 0; index < array.length - 1; index++) {
+        for (let i = 0; i < array.length - 1; i++) {
+            let minIndex = i;
+            for (let j = i + 1; j < array.length; j++) {
                 compareCount++;
-                if (compare(array[index], array[index + 1])) {
-                    swapCount++;
-                    let tempElem = array[index];
-                    array[index] = array[index + 1];
-                    array[index + 1] = tempElem;
+                if (compare(array[j], array[minIndex])) {
+                    minIndex = j;
                 }
             }
-        }
-        console.log(`Compare count: ${compareCount}`);
-        console.log(`Swap count: ${swapCount}`);
-        return array;
-    }
-
-    SortLibrary.selectionSort = function (array, reversed) {
-        const compare = getCompare(!reversed);
-        let compareCount = 0;
-        let swapCount = 0;
-        for (let index = 0; index < array.length - 1; index++) {
-            let min = index;
-            for (let current = index + 1; current < array.length; current++) {
-                compareCount++;
-                if (compare(array[current], array[min])) {
-                    min = current;
-                }
-            }
-            swapCount++;
-            let tempElem = array[index];
-            array[index] = array[min];
-            array[min] = tempElem;
-        }
-        console.log(`Compare count: ${compareCount}`);
-        console.log(`Swap count: ${swapCount}`);
-        return array;
-    }
-
-
-    SortLibrary.insertionSort = function (array, reversed) {
-        const compare = getCompare(reversed);
-        let compareCount = 0;
-        let swapCount = 0;
-        for (let index = 1; index < array.length; index++) {
-            let left = index - 1;
-            if(isSorted(array, reversed)){
-                break;
-            }
-            while (left > -1 && compare(array[left], array[left + 1])) {
-                compareCount++;
+            if (i !== minIndex) {
+                swap(array, i, minIndex);
                 swapCount++;
-                let tempElem = array[left + 1];
-                array[left + 1] = array[left];
-                array[left] = tempElem;
-                left--;
             }
         }
-        console.log(`Compare count: ${compareCount}`);
-        console.log(`Swap count: ${swapCount}`);
+        console.log(`Selection sort compare count: ${compareCount}`);
+        console.log(`Selection sort swap count: ${swapCount}`);
         return array;
-    }
+    };
+
+    SortLibrary.insertionSort = function(array, ascending = true) {
+        const compare = ascending ? (a, b) => a > b : (a, b) => a < b;
+        let compareCount = 0;
+        let swapCount = 0;
+        for (let i = 1; i < array.length; i++) {
+            let currentValue = array[i];
+            let j = i - 1;
+            while (j >= 0 && compare(array[j], currentValue)) {
+                compareCount++;
+                array[j + 1] = array[j];
+                swapCount++;
+                j--;
+            }
+            array[j + 1] = currentValue;
+        }
+        console.log(`Insertion sort compare count: ${compareCount}`);
+        console.log(`Insertion sort swap count: ${swapCount}`);
+        return array;
+    };
+
+
+    let array = generateArray(100);
+
+    console.log(SortLibrary.bubbleSort([...array], true));
+    console.log(SortLibrary.bubbleSort([...array]));
+    console.log(SortLibrary.selectionSort([...array], true));
+    console.log(SortLibrary.selectionSort([...array]));
+    console.log(SortLibrary.insertionSort([...array], true));
+    console.log(SortLibrary.insertionSort([...array]));
+})(window);
+
 
     global.SortLibrary = SortLibrary;
 })(window);
