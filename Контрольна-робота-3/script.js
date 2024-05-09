8c50162a37882b18920a4b57068bd5e91962def0
Diana-Magas <155903433+Diana-Magas@users.noreply.github.com>
2024-05-09 18:34:21 +0300

    Update script.js

--- Контрольна-робота-3/script.js
+++ Контрольна-робота-3/script.js
@@ -88,7 +88,8 @@ document.addEventListener("DOMContentLoaded", function() {
       resultHeading.textContent = "Result: Operand 1 is less or equal to 0";
     } else {
       fetchData('JSON/log.json', function(data) {
-      resultHeading.textContent = "Result: " + result;
+        let result = Math.log(operand1);
+        resultHeading.textContent = "Result: " + result;
         logInfoDiv.innerHTML = `<strong>${data.name}</strong><br><img src="${data.image_name}" alt="${data.name}"><br>Description: ${data.description}`;
       });
     }
@@ -98,7 +99,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let degrees = parseFloat(op1Input.value);
     let radians = degrees * (Math.PI / 180); 
     fetchData('JSON/sin.json', function(data) {
-       resultHeading.textContent = "Result: " + result;
+      let result = Math.sin(radians);
+      resultHeading.textContent = "Result: " + result;
       sinInfoDiv.innerHTML = `<strong>${data.name}</strong><br><img src="${data.image_name}" alt="${data.name}"><br>Description: ${data.description}`;
     });
   });
@@ -107,7 +109,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let degrees = parseFloat(op1Input.value);
     let radians = degrees * (Math.PI / 180); 
     fetchData('JSON/tan.json', function(data) {
-    resultHeading.textContent = "Result: " + result;
+      let result = Math.tan(radians);
+      resultHeading.textContent = "Result: " + result;
       tanInfoDiv.innerHTML = `<strong>${data.name}</strong><br><img src="${data.image_name}" alt="${data.name}"><br>Description: ${data.description}`;
     });
   });
