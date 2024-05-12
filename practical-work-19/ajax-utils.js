a100b347bd8e3e8ee8ebb7e09627ae164117ba18
Diana-Magas <155903433+Diana-Magas@users.noreply.github.com>
2024-05-12 19:13:51 +0300

    Update ajax-utils.js

--- practical-work-19/ajax-utils.js
+++ practical-work-19/ajax-utils.js
@@ -1,37 +1,38 @@
-const ajaxUtils = {
-    getRequestObject: function() {
-        if (window.XMLHttpRequest) {
+(function (global) {
+    let ajaxUtils = {};
+
+    function getRequestObject() {
+        if (global.XMLHttpRequest) {
             return new XMLHttpRequest();
         } else {
-            window.alert("Ajax is not supported");
+            global.alert("Ajax is not supported");
             return null;
         }
-    },
-    
-    sendGetRequest: function(requestUrl, responseHandler, isJSON) {
-        let request = this.getRequestObject();
+    }
+
+    ajaxUtils.sendGetRequest = function (requestUrl, responseHandler, isJSON) {
+        let request = getRequestObject();
         request.onreadystatechange = function () {
             handleResponse(request, responseHandler, isJSON);
         };
         request.open("GET", requestUrl, true);
         request.send(null);
-    },
-    
-    handleResponse: function(request, responseHandler, isJSON = true) {
+    };
+
+    function handleResponse(request, responseHandler, isJSON = true) {
         if (request.readyState == 4 && request.status == 200) {
             responseHandler(isJSON ? JSON.parse(request.responseText) : request.responseText);
         }
-    },
-    
-    sendPostRequest: function(requestUrl, requestBody, responseHandler) {
-        let request = this.getRequestObject();
+    }
+
+    ajaxUtils.sendPostRequest = function (requestUrl, requestBody, responseHandler) {
+        let request = getRequestObject();
         request.onreadystatechange = function () {
             handleResponse(request, responseHandler);
         };
         request.open("POST", requestUrl, true);
         request.send(requestBody);
-    }
-};
+    };
 
-// Експортуємо об'єкт ajaxUtils
-export { ajaxUtils };
+    global.ajaxUtils = ajaxUtils;
+})(window);
