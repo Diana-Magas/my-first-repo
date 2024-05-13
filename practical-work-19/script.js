88f638f33db56506b84c859ff23825855a8e3363
Diana-Magas <155903433+Diana-Magas@users.noreply.github.com>
2024-05-13 15:34:51 +0300

    Update script.js

--- practical-work-19/script.js
+++ practical-work-19/script.js
@@ -1,31 +1,28 @@
-import "./humburger.js"; 
+import "./humburger.js";
 import "./ajax-utils.js";
-import "./carusel.js";
+import carusel from "./carusel.js";
 
 (function (global) {
     let Content = {};
     const homeHTML = "./snippets/home-snippet.html";
-    const ContainerSelector = ".container";
+    const contentContainerSelector = ".container";
 
     const insertHTML = (selector, html) => {
         document.querySelector(selector).innerHTML = html;
     };
 
     document.addEventListener("DOMContentLoaded", (event) => {
-        showLoading(ContainerSelector); 
+        showLoading(containerSelector);
         setTimeout(() => {
             ajaxUtils.sendGetRequest(
                 homeHTML,
                 (response) => {
-                    insertHTML(ContainerSelector, response);
-                    carusel(); 
+                    insertHTML(containerSelector, response);
+                    carousel();
                 },
                 false
             );
         }, 3750);
     });
-
     global.Content = Content;
 })(window);
-
-
